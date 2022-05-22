31f21ebaca67d8dee2b5ed6f971b96a0cb9ba719
Your Name <you@example.com>
2022-05-22 15:01:56 +0530

    mobile navigation added

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -13,14 +13,25 @@ function redirectTo(n) {
 const trigger = document.querySelector(".contact-us-btn");
 const close_button = document.querySelector(".close-button");
 
+trigger.addEventListener("click", toggleModel);
+close_button.addEventListener("click", toggleModel);
+
 function toggleModel() {
     if (!(window.innerWidth < 1260)) {
         document.getElementsByClassName("model-background")[0].classList.toggle("hide-model");
+        document.getElementsByClassName("modelBox")[0].classList.toggle("animate-box");
     }
+
+}
+
+function toggleMobileNav() {
+    document.getElementsByClassName("mobile-nav-background")[0].classList.toggle("show-mobile-nav");
+}
+
+function animateRedirectTo(n) {
+    toggleMobileNav();
+    setTimeout(redirectTo, 360, n);
 }
 
 function sendMail() {
 }
-
-trigger.addEventListener("click", toggleModel);
-close_button.addEventListener("click", toggleModel);
